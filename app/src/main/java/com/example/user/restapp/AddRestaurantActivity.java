0630b7a0c3d8866e1e6c32cbce82a3f06a435859
bayan <bayan.negem@hotmail.com>
2018-03-11 11:56:05 +0200

    hello
    first commit

---
+++ app/src/main/java/com/example/user/restapp/AddRestaurantActivity.java
@@ -0,0 +1,86 @@
+package com.example.user.restapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class AddRestaurantActivity extends AppCompatActivity {
+
+    private EditText etReastaurantName;
+    private EditText etDescription;
+    private EditText etPhone;
+    private EditText etWebsite;
+    private EditText etFacebook;
+    private EditText etWhatsapp;
+    private EditText etLocation;
+    private Button btnAddRest;
+    FirebaseDatabase database;
+    DatabaseReference ref;
+    DatabaseReference restsRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_restaurant);
+
+        init();
+    }
+
+    private void init() {
+        etReastaurantName = (EditText)findViewById(R.id.etReastaurantName);
+        etDescription = (EditText)findViewById(R.id.etDescription);
+        etPhone = (EditText)findViewById(R.id.etPhone);
+        etWebsite =  (EditText)findViewById(R.id.etWebsite);
+        etFacebook =  (EditText)findViewById(R.id.etFacebook);
+        etWhatsapp =  (EditText)findViewById(R.id.etWhatsapp);
+        etLocation =  (EditText)findViewById(R.id.etLocatiom);
+        btnAddRest =  (Button)findViewById(R.id.btnAddRest);
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference();
+        restsRef = ref.child("restaurants");
+        restsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void addRest(View view)
+    {
+        String name = etReastaurantName.getText().toString();
+        String desc = etDescription.getText().toString();
+        String phone = etPhone.getText().toString();
+        String web = etWebsite.getText().toString();
+        String face = etFacebook.getText().toString();
+        String whats = etWhatsapp.getText().toString();
+        String locate = etLocation.getText().toString();
+        if (!name.isEmpty() && !desc.isEmpty() && !phone.isEmpty() && !web.isEmpty() &&
+                !face.isEmpty() && !whats.isEmpty() && !locate.isEmpty() )
+        {
+            Restaurant rest = new Restaurant(name, desc, phone ,web, face, whats, locate);
+            restsRef.push().setValue(rest);
+        }
+        else
+        {
+            Toast.makeText( AddRestaurantActivity.this, R.string.missing_data,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/user/restapp/Restaurant.java
@@ -0,0 +1,98 @@
+package com.example.user.restapp;
+
+/**
+ * Created by USER on 07/03/2018.
+ */
+
+public class Restaurant {
+
+    private String name;
+    private String description;
+    private String phone;
+    private String website;
+    private String faceebook;
+    private String whatsapp;
+    private String location;
+
+    public Restaurant() {
+    }
+
+    public Restaurant(String name, String description, String phone, String website, String faceebook, String whatsapp, String location) {
+        this.name = name;
+        this.description = description;
+        this.phone = phone;
+        this.website = website;
+        this.faceebook = faceebook;
+        this.whatsapp = whatsapp;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getFaceebook() {
+        return faceebook;
+    }
+
+    public void setFaceebook(String faceebook) {
+        this.faceebook = faceebook;
+    }
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", phone='" + phone + '\'' +
+                ", website='" + website + '\'' +
+                ", faceebook='" + faceebook + '\'' +
+                ", whatsapp='" + whatsapp + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
